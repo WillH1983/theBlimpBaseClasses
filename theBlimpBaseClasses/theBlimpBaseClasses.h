d4f15b4fecbfabbddf601c050566bb8b1add381b
Will Hindenburg <WillH1983@yahoo.com>
2012-08-11 13:58:40 -0500

    Initial Commit

---
+++ theBlimpBaseClasses/theBlimpBaseClasses.h
@@ -0,0 +1,13 @@
+//
+//  theBlimpBaseClasses.h
+//  theBlimpBaseClasses
+//
+//  Created by Will Hindenburg on 8/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface theBlimpBaseClasses : NSObject
+
+@end
---
+++ theBlimpBaseClassesTests/theBlimpBaseClassesTests.m
@@ -0,0 +1,32 @@
+//
+//  theBlimpBaseClassesTests.m
+//  theBlimpBaseClassesTests
+//
+//  Created by Will Hindenburg on 8/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "theBlimpBaseClassesTests.h"
+
+@implementation theBlimpBaseClassesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in theBlimpBaseClassesTests");
+}
+
+@end
